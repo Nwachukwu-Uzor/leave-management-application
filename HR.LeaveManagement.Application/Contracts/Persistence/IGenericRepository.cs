33b5652253eeb7a36d9617ee7f501d75fed073e7
Uzor-Nwachukwu <uzor1997@gmail.com>
2023-12-30 15:25:19 +0100

    add: core project base implementation

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T> Create(T entity);
+    Task<T> Update(T entity);
+    Task<T> Delete(T entity);
+    Task<List<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+}
+
